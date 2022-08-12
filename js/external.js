846d2b9f2b25969a0abad2f72168df70ab1ac6b6
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-08-12 12:32:58 -0500

    javascript with html exercises

---
+++ js/external.js
@@ -0,0 +1,46 @@
+//alert("Welcome to my Website!");
+
+//let favoriteColor = prompt("What's your favorite color?");
+//alert(favoriteColor + " is my favorite color too!");
+
+// let daysOfLittleMermaid = prompt("How many days did you rent The Little Mermaid for?");
+// let daysOfBrotherBear = prompt("How many days did you rent Brother Bear for?");
+// let daysOfHercules = prompt("How many days did you rent Hercules for?");
+//
+// let totalCost = (daysOfLittleMermaid * 3) + (daysOfBrotherBear * 3) + (daysOfHercules * 3);
+//
+// alert("Your total rental cost is " + totalCost + " dollars");
+
+// let hoursAtGoogle = prompt("How many hours did you work at google?");
+// let payAtGoogle = prompt("What is your hourly pay rate at google?");
+// let hoursAtAmazon = prompt("How many hours did you work at amazon?");
+// let payAtAmazon = prompt("What is your hourly pay rate at amazon?");
+// let hoursAtFacebook = prompt("How many hours did you work at facebook?");
+// let payAtFacebook = prompt("What is your hourly pay rate at facebook?");
+//
+// let totalPay = hoursAtGoogle * payAtGoogle + hoursAtAmazon * payAtAmazon + hoursAtFacebook * payAtFacebook;
+// alert("Your total pay is: " + totalPay + " dollars");
+
+// let classFull = confirm("Is the class full?");
+// let scheduleConflict = confirm("Is there a scheduling conflict?");
+//
+// let canEnroll = classFull && scheduleConflict;
+//
+// if(canEnroll) {
+//     alert("You can enroll")
+// } else {
+//     alert("You can't enroll")
+// }
+
+// let numberOfItemsBought = prompt("How many items did the customer buy?");
+// let offerExpired = confirm("has the offer expired?");
+// let premiumMember = confirm("Is the customer a premium member?");
+//
+// let makeProductOffer = (premiumMember || numberOfItemsBought > 2) && !offerExpired;
+//
+// if (makeProductOffer){
+//     alert("You get a product offer!")
+// } else {
+//     alert("You are third a third class customer")
+// }
+
---
+++ js/javascript-and-HTML.js
@@ -0,0 +1,13 @@
+//alert("yooo")
+
+// let userChoice = confirm("Are you sure you don't want to buy stuff?");
+// console.log(userChoice);
+//
+// if (userChoice) {
+//     alert("Yay!");
+// } else {
+//     alert("Boo :(");
+//}
+
+let name = prompt("Whats your name?");
+console.log(name);
