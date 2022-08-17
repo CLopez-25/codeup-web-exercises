ac0cb28352b7a56fc63ed000bbcfebb4c040fef2
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-08-17 15:20:21 -0500

    loops-lecture and loops exercises

---
+++ js/loops-lecture.js
@@ -0,0 +1,87 @@
+// let slices = 4;
+// console.log(`There are ${slices} slices`);
+// while (slices > 0){
+//     console.log("I'll have a slice");
+//     slices = slices - 1;
+//     //shorthand = slices--;
+//     console.log(`Now there's ${slices} slices left`);
+// }
+// console.log("No more pizza.");
+// There's 4 slices, code continues to loop until there is no pizza left.
+// console log at the beginning and the end to avoid breaking your code!
+// Executing the code until 1 because we put > 0.
+
+// let counter = 0;
+// while (counter <= 100){
+//     console.log(counter);
+//     counter++;
+// }
+//this will count from 0 to 100.
+
+// let counter = 100;
+// while (counter >= 0){
+//     console.log(counter);
+//     counter--;
+// }
+//this will count from 100 to 0.
+
+// let number = 5;
+// while (number < 200){
+//     // number = number * 2;
+//     console.log(number)
+//     number *= 2;
+// }
+// this multiplying the number output by 2, starting from 5 for numbers less than 200.
+
+//Pseudo code
+// Ask a user for input -- how much does something cost
+// Assign the user input to a variable
+// Declare a variable to hold the total cost.
+// Tell the user what the total is
+// Get the user input for the next item's cost
+// Needs to be a way for the user to say when there's no more items
+
+//first draft:
+// let total = 0;
+// let priceOfItem = parseFloat(prompt("Enter the price of your item:"));
+// total = total + priceOfItem;
+// alert("Your total is now " + total);
+// let userInput = "";
+// while (userInput !== "stop"){
+//     userInput = prompt("Enter the price of your next item:");
+//     if (userInput === "stop"){
+//         alert("Your final total is " + total);
+//     } else{
+//         priceOfItem = parseFloat(userInput);
+//         total = total + priceOfItem;
+//         alert("Your total is now " + total);
+//     }
+// }
+
+
+// if i want a loop to continue i can (while) true.
+// we are using a conditional inside of a loop.
+// Short version:
+
+// let total = 0;
+// let priceOfItem;
+// while(true){
+//     let userInput = prompt("Enter the price of your item:" +
+//                             "\n Enter STOP when you are done");
+//     if (userInput === "STOP") {
+//         alert("Your final total is " + total.toFixed(2));
+//         break;
+//     } else {
+//         priceOfItem = parseFloat(userInput);
+//         total = total + priceOfItem;
+//         alert("Your total is " + total.toFixed(2));
+//     }
+// }
+
+let number = Math.ceil(Math.random() * 6);
+let guess;
+do {
+    guess = parseInt(prompt("Enter a number between 1 and 6"));
+    console.log(guess)
+} while (guess !== number);
+alert(`Your guess of ${guess} matches the number ${number}!`);
---
+++ js/while.js
@@ -0,0 +1,20 @@
+// let number = 2;
+// while (number <= 65556){
+//     console.log(number);
+//     number *= 2;
+// };
+
+let allCones = Math.floor(Math.random() * 51) + 50;
+
+do {
+    let conesWanted = Math.floor(Math.random() * 5) + 1;
+    if (conesWanted > allCones){
+        console.log(`cannot sell you ${conesWanted} I only have ${allCones}`);
+        allCones -= allCones - conesWanted;
+    } else {
+        console.log(`${conesWanted} sold...`);
+        allCones = allCones - conesWanted;
+    }
+    console.log(`${allCones} left`);
+} while (allCones > 0);
+console.log("Yay! I sold all!!");
