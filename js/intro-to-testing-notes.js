74513426127fbddde70c2c232b52106491b8527f
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-08-23 11:05:32 -0500

    Intro to testing Notes

---
+++ js/intro-to-testing-notes.js
@@ -0,0 +1,17 @@
+// describe- takes in two parameters
+// it function- takes in a message/title
+// also takes in a function as a second parameter
+// translate plain english into jasmine specs(expectation)
+// expectation has two parts an assertion and a matcher
+// expect is the assertion/ toBe-not.ToBe is a matcher
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
