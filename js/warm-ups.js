a801c7e00dfe486b269d2b56375ddd7ac1684be8
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-08-25 10:36:08 -0500

    Warm ups complete

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -1,9 +1,30 @@
 //write a function that accepts an array of numbers and returns its average.
+let array = [5, 10, 15];
+let array2 = [-25, -22, 250, 60];
+let array3 = ["not a number", 25, 78];
 
+function arrayAverage(array){
+    let total = 0;
+    for(let i = 0; i < array.length; i++){
+        if (!isNumber(array[i])){return false;}
+        total += array[i];
+    }
+    return total/array.length;
+}
 
 
 //function that accepts an array of strings and returns the longest string. if there are two strings of equal length
 // it returns the one with the lowest index.
+
+let stringArray = ["cesar", "sky", "oliver"];
+
+function longestString(arrayOfStrings){
+    let longest = '';
+    for (let i = 0; i < arrayOfStrings.length; i++){
+        if (arrayOfStrings[i].length > longest.length)longest = arrayOfStrings[i];
+        }
+    return longest;
+    }
 
 function indexOfSmallest(a) {
     let lowest = 0;
