3a4794811f7b9350feccfb0a6b1a508e6b1b1534
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-02 16:22:23 -0500

    Codewars question

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -180,3 +180,22 @@ function beeramid (bonus,price){
     }
     return x;
 }
+
+let shoes = [
+    [0, 21],
+    [1, 23],
+    [1, 21],
+    [0, 23]
+];
+
+function pairOfShoes(shoes){
+    let firstShoeHasMatch = false;
+    let secondShoeHasMatch = false;
+    for (let i = 1; i < shoes.length; i++){
+        if (shoes[i][1] === shoes[0][1]){
+            if (shoes[i][0] !== shoes[0][0])
+            firstShoeHasMatch = true;
+        }
+    }
+    return firstShoeHasMatch;
+}
