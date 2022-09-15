9cd7cdc7c473e91b5f12be64b9cce6b9175c0d20
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-15 15:03:03 -0500

    added button and alert to dom events lecture

---
+++ js/dom-events-lecture.js
@@ -0,0 +1,21 @@
+// the three steps of handling events
+// #1 get a reference to the element that will trigger the event
+
+let theButton = document.querySelector("button");
+
+// #2 write a function that will be triggered by the event
+// a function is a set of instructions. this kind of function that is triggered by an event is called an event handler, or also called callback
+// the function contains the instruction that will run when the event is triggered
+
+function eventHandler(){
+    alert("You clicked me!!");
+}
+
+// #3 register the handler
+
+// we can use addEventListener with two arguments: the type of event and the function that will be triggered
+
+theButton.addEventListener('click', eventHandler);
+
+// below also works:
+// theButton.onclick = eventHandler;
