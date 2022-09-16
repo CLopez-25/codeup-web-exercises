15746a8e53477d4bf4598d45e76a70b62d9bcd70
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-16 10:14:13 -0500

    went over examples of different events

--- js/dom-events-lecture.js
+++ js/dom-events-lecture.js
@@ -27,6 +27,38 @@
 
 
 // MINI EXERCISE:
-document.querySelector("div").addEventListener('click', function (){
-    alert("I am a square");
+
+// document.querySelector(".square").addEventListener('click', function(){
+//     alert("I am a square");
+// })
+// and my color is " + window.getComputedStyle(document.querySelector(".square"), null).getPropertyValue('background-color')
+
+let theSquare = document.querySelector('.square');
+
+let theHandler = function(){
+    // alert("I am a square " + window.getComputedStyle(document.querySelector(".square"), null).getPropertyValue('background-color'));
+}
+
+theSquare.addEventListener('mousemove', function(e){
+    // console.log(e.target.className);
+    // let theColor = getComputedStyle(e.target).backgroundColor
+    // console.log(theColor);
+    // document.getElementById("anotherSquare").style.backgroundColor = theColor;
+    alert("Event!");
 });
+
+let theButton = document.querySelector('button');
+
+document.querySelector('body').addEventListener('resize', function(event){
+    alert("Free Flashlights! Just sign up for email newsletter!");
+    // console.log(getComputedStyle(event.target).backgroundColor);
+})
+
+theButton.onclick = buttonClickFunction;
+
+// Get the user input from a form field
+let formInput = document.getElementById("formInput");
+
+formInput.onkeyup = function(){
+    console.log(formInput.value);
+}
