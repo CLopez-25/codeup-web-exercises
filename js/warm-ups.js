ae8b1b2270edce108bfdaefff1d36a19f4fda323
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-19 13:04:18 -0500

    update to warmups

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -1,4 +1,4 @@
-// //write a function that accepts an array of numbers and returns its average.
+//write a function that accepts an array of numbers and returns its average.
 // let array = [5, 10, 15];
 // let array2 = [-25, -22, 250, 60];
 // let array3 = ["not a number", 25, 78];
@@ -11,11 +11,11 @@
 //     }
 //     return total/array.length;
 // }
-//
-//
-// //function that accepts an array of strings and returns the longest string. if there are two strings of equal length
-// // it returns the one with the lowest index.
-//
+
+
+//function that accepts an array of strings and returns the longest string. if there are two strings of equal length
+// it returns the one with the lowest index.
+
 // let stringArray = ["cesar", "sky", "oliver"];
 //
 // function longestString(arrayOfStrings){
@@ -82,44 +82,44 @@
 //
 // console.log(convertToObject("Intro to Programming", 20));
 
-const neighborhood1 ={
-    neighborhood: "Lovely Estates",
-    medianHomePrice: 280000,
-    pool: true,
-    tennis: false,
-    crimeRate: "low",
-    schools: [
-        {name: "ES1", rating: 8},
-        {name: "MS2", rating: 6},
-        {name: "HS3", rating: 8}
-    ]
-}
-
-const neighborhood2 ={
-    neighborhood: "Luminous Estates",
-    medianHomePrice: 270000,
-    pool: true,
-    tennis: false,
-    crimeRate: "low",
-    schools: [
-        {name: "ES1", rating: 8},
-        {name: "MS2", rating: 8},
-        {name: "HS3", rating: 8}
-    ]
-}
-
-const neighborhood3 ={
-    neighborhood: "Ginormous Ego Estates",
-    medianHomePrice: 350000,
-    pool: true,
-    tennis: true,
-    crimeRate: "low",
-    schools: [
-        {name: "ES1", rating: 9},
-        {name: "MS2", rating: 9},
-        {name: "HS3", rating: 9}
-    ]
-}
+// const neighborhood1 ={
+//     neighborhood: "Lovely Estates",
+//     medianHomePrice: 280000,
+//     pool: true,
+//     tennis: false,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 8},
+//         {name: "MS2", rating: 6},
+//         {name: "HS3", rating: 8}
+//     ]
+// }
+//
+// const neighborhood2 ={
+//     neighborhood: "Luminous Estates",
+//     medianHomePrice: 270000,
+//     pool: true,
+//     tennis: false,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 8},
+//         {name: "MS2", rating: 8},
+//         {name: "HS3", rating: 8}
+//     ]
+// }
+//
+// const neighborhood3 ={
+//     neighborhood: "Ginormous Ego Estates",
+//     medianHomePrice: 350000,
+//     pool: true,
+//     tennis: true,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 9},
+//         {name: "MS2", rating: 9},
+//         {name: "HS3", rating: 9}
+//     ]
+// }
 
 // Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
 
@@ -172,37 +172,37 @@ const neighborhood3 ={
 //     return x;
 // }
 
-function beeramid (bonus,price){
-    let total = (bonus - (bonus % price))/price; //this line tells you how many cans of beer you can buy
-    let x = 0; //accumulator variable
-    for (let i = 0; total >= ((1 / 3)*(i**3))+((1 / 2)*(i**2))+((1 / 6)*(i)); i++){ //solved for a cubic function
-        x = i; //match the accumulator to the number of levels
-    }
-    return x;
-}
+// function beeramid (bonus,price){
+//     let total = (bonus - (bonus % price))/price; //this line tells you how many cans of beer you can buy
+//     let x = 0; //accumulator variable
+//     for (let i = 0; total >= ((1 / 3)*(i**3))+((1 / 2)*(i**2))+((1 / 6)*(i)); i++){ //solved for a cubic function
+//         x = i; //match the accumulator to the number of levels
+//     }
+//     return x;
+// }
+//
+// let shoes = [
+//     [0, 21],
+//     [1, 23],
+//     [1, 21],
+//     [0, 23]
+// ];
 
-let shoes = [
-    [0, 21],
-    [1, 23],
-    [1, 21],
-    [0, 23]
-];
-
-function pairOfShoes(shoes) {
-    let shoeHasMatch = false;
-    let shoeThatMatches;
-    let newArray;
-    for (let i = 1; i < shoes.length; i++){
-        if (shoes[i][1] === shoes[0][1]){
-            if (shoes[i][0] !== shoes[0][0]){
-                shoeHasMatch = true;
-                shoes.splice(i, 1);
-                shoes.splice(0,1);
-            }
-        }
-    }
-    return shoes.length < 2 || !shoeHasMatch ? shoeHasMatch : pairOfShoes(shoes);
-}
+// function pairOfShoes(shoes) {
+//     let shoeHasMatch = false;
+//     let shoeThatMatches;
+//     let newArray;
+//     for (let i = 1; i < shoes.length; i++){
+//         if (shoes[i][1] === shoes[0][1]){
+//             if (shoes[i][0] !== shoes[0][0]){
+//                 shoeHasMatch = true;
+//                 shoes.splice(i, 1);
+//                 shoes.splice(0,1);
+//             }
+//         }
+//     }
+//     return shoes.length < 2 || !shoeHasMatch ? shoeHasMatch : pairOfShoes(shoes);
+// }
 
 //     function pairOfShoes(shoes) {
 //
@@ -223,3 +223,80 @@ function pairOfShoes(shoes) {
 //     }
 //     return true;
 // }
+
+
+const attendance = {
+    miami: 32789,
+    buffalo: 45678,
+    portland: 24567
+}
+// Given the attendance object, write a function that returns the total attendance across all teams.
+
+// function totalAttendance(attendanceObject){
+//     let attendanceArray = Object.values(attendanceObject);
+//     let total = 0;
+//     for (let i = 0; i < attendanceArray.length; i++){
+//         total += attendanceArray[i];
+//     }
+//     return total;
+// }
+
+function totalAttendance(attendanceObject){
+    let total = 0;
+    for (const prop in attendance) {
+        total += attendance[prop];
+    }
+    return total;
+}
+
+// Given the attendance object, write a function that returns the average attendance
+function averageAttendance(attendanceObject){
+    let total = 0;
+    for (const prop in attendance){
+        total += attendance[prop];
+    }
+    return total/Object.keys(attendance).length;
+}
+
+// Given the attendance object, write a function that returns an object with totalAttendance and averageAttendance properties set to the correct values
+
+function createObject(attendanceObject){
+    let total = 0;
+    for (const prop in attendanceObject){
+        total += attendance[prop];
+    }
+    return {
+        totalAttendance : total,
+        averageAttendance: total/Object.keys(attendance).length
+    }
+}
+
+// Convert to Number
+//  * You prepare a list to send to the insurance company. As you finish, you notice you miss-formatted it.
+//  * Write a function named convertToNumber that takes in an object with at least one key/value pair, convert all the values to numbers.
+//  *
+//  * Examples
+//  *
+//  * convertToNumber({ piano: "200" }) ➞ { piano: 200 }
+//  * convertToNumber({ piano: "200", tv: "300" }) ➞ { piano: 200, tv: 300 }
+//  * convertToNumber({ piano: "200", tv: "300", stereo: "400" }) ➞ { piano: 200, tv: 300, stereo: 400 }
+//  */
+
+
+/**
+ * Online Shopping
+ * Create a function named freeShipping that determines whether a shopping order is eligible for free shipping. An order is eligible for free shipping if the total cost of items purchased exceeds $50.00.
+ *
+ * Examples
+ *
+ * freeShipping({ Shampoo: 5.99, Rubber_Ducks: 15.99 }) ➞ false
+ * freeShipping({ Flatscreen_TV: 399.99 }) ➞ true
+ * freeShipping({ Monopoly: 11.99, Secret_Hitler: 35.99, Bananagrams: 13.99 }) ➞ true
+ *
+ * Notes
+ * Ignore tax or additional fees when calculating the total order cost.
+ */
+
+function freeShipping(){
+
+}
