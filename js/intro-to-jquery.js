61ab97a56780d8b826d344ea9a14dad39b65b7a7
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-20 15:51:19 -0500

    finished intro to jquery lecture

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -1,5 +1,16 @@
-$("h3").click(function (){
-    //takes in a selector and returns an object that represents elements in a page
-    $(this).next().slideToggle(500);
-});
-// to call the jquery function $ can be used as a shortcut and can be used more than once
+// $(document).ready(function (){
+//     $("h3").click(function (){
+//         //takes in a selector and returns an object that represents elements in a page
+//         $(this).next().slideToggle(500);
+//     });
+// // to call the jquery function $ can be used as a shortcut and can be used more than once
+// });
+// // it will wait for the document to be loaded first and then it will run the function
+
+function init() {
+    $("h3").click(function (){
+        $(this).next().slideToggle(500);
+    });
+}
+
+window.onload = init;
