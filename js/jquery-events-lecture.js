3b6730fe15e7af71367db4795c20646f79c122e2
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-21 13:32:06 -0500

    updated events lecture

--- js/jquery-events-lecture.js
+++ js/jquery-events-lecture.js
@@ -7,5 +7,25 @@ $(function (){
       $(this).after("<button class='will-this-work>'>Click Me</button>"); 
    });
    
-   
+   // this won't work because you are trying to register an event handler with an element that doesnt exist yet:
+   // $(".will-this-work").on('click', function (){
+   //     alert(":( this didnt work");
+   // });
+    
+    // this will work/ this is a delegated event
+    $("#dynamic-content").on('click', '.will-this-work', function (){
+       alert("ooooooooo yeeeeeeeeaaaaahhhhh");
+    });
+    
+    $(".box").hover(function(){
+       $(this).addClass("hotpink");
+    }, 
+     function(){
+        $(this).removeClass("hotpink");
+     });
+    
+    $(document).keydown(function(){
+        alert("you just pressed a key down");
+    });
+    
 });
