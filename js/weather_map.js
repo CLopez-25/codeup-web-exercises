df3e3acaf3f2b74964080ca42ae7120f097b30ee
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-27 16:03:58 -0500

    updated weathermap js

--- js/weather_map.js
+++ js/weather_map.js
@@ -26,17 +26,6 @@
         units: "imperial"
         }).done(function(data) {
         console.log(data);
-            $('#weather-cards').append(`<div>
-                    <div class="card-header">
-                        ${formatTime(data[7].dt)}
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item text-center">${data.main.temp_max} / ${data.main.temp_min}</li>
-                        <li class="list-group-item">Humidity: ${data.main.humidity}</li>
-                        <li class="list-group-item">Wind: ${data.wind.speed}</li>
-                        <li class="list-group-item">Pressure: ${data.main.pressure}</li>
-                    </ul>
-                </div>`);
         });
         
     });
