51a2c85026b5622f0f9ee839cbaf381394164ba1
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-09-29 02:55:17 -0500

    added zoom bar to map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,13 +1,5 @@
 $(function () {
-    // $.get("http://api.openweathermap.org/data/2.5/weather", {
-    //     APPID: OPEN_WEATHER_APPID,
-    //     lat: 29.423017,
-    //     lon: -98.48527,
-    //     units: "imperial"
-    // }).done(function (data) {
-    //     console.log(data);
-    // });
-
+    // The weathermap API
     $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WEATHER_APPID,
         lat: 29.423017,
@@ -16,7 +8,8 @@ $(function () {
     }).done(function (data) {
         printWeatherCards(data);
     });
-
+    
+    //This is my function for displaying my weather cards with the current weather data inside them
     function printWeatherCards(data) {
         $("#card").html('');
         for (let i = 0; i < data.list.length; i++) {
@@ -41,20 +34,23 @@ $(function () {
             }
         }
     }
-
+    
+    // This is displays the map with the current location being San Antonio
     mapboxgl.accessToken = MAPBOX_API_TOKEN;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/dark-v10', // style URL
+        style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
         center: [-98.4916, 29.4252], // starting position [lng, lat]
         zoom: 3, // starting zoom
         projection: 'globe' // display the map as a 3D globe
     });
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
+        const nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-right');
     });
 
-
+    // This code has my starting marker displayed on San Antonio and when the user searches a new location it creates a new marker and displays the forecast for that location as well as updates the current location text in the navbar.
     const codeupMarker = new mapboxgl.Marker({color: 'black', draggable: true})
         .setLngLat([-98.48962, 29.42784])
         .addTo(map);
@@ -78,6 +74,7 @@ $(function () {
         });
     });
 
+    // This code creates a new marker when the user clicks on a new location, and it displays the forecast for that location as well as updates the current location text in the navbar.
     map.on('click', (e) => {
         $(".mapboxgl-marker").remove();
         const userMarker = new mapboxgl.Marker({color: 'black', draggable: true}).setLngLat(e.lngLat).addTo(map);
