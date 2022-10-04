3a3f4d89c04f130bbbc3bfffb6f1a7db5fa49460
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-04 14:06:52 -0500

    completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,11 +1,17 @@
- fetch('https://api.github.com/users/CLopez-25/events', {
+ fetch('https://api.github.com/users/CLopez-25/events/public', {
     headers: {
         'Authorization': 'token ' + GITHUB_PROMISES_TOKEN
     }
 }).then( response => response.json())
-    .then( date => console.log(date[0].created_at))
-    .catch( error => console.error(error));
+    .then( date => console.log(new Date(date[0].created_at).toDateString()));
 
-function wait(number){
-
-}
+ const wait = ms => {
+     return new Promise(response => {
+         setTimeout(() => {
+             response(`You'll see this after ${ms/1000} seconds`);
+         }, ms);
+     })
+ }
+ wait(1000).then(message => console.log(message));
+ wait(3000).then(message => console.log(message));
+ 
