18a089c6cf64ca71323c22d68a50500d0299bdb5
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-04 12:15:23 -0500

    completed up to number three of exercise

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,11 @@
-fetch('https://api.github.com/users/CLopez-25', {
+ fetch('https://api.github.com/users/CLopez-25/events', {
     headers: {
         'Authorization': 'token ' + GITHUB_PROMISES_TOKEN
     }
 }).then( response => response.json())
-    .then( username => console.log(username))
+    .then( date => console.log(date[0].created_at))
     .catch( error => console.error(error));
 
+function wait(number){
+
+}
