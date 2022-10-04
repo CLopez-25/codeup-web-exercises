3b4ae234eb4ed9bb90e3411171d74791cdd5056d
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-10-04 16:03:48 -0500

    Completed restful api lecture

--- js/promises.js
+++ js/promises.js
@@ -5,6 +5,18 @@
 }).then( response => response.json())
     .then( date => console.log(new Date(date[0].created_at).toDateString()));
 
+ // jason solution
+ // async function getUserLastCommitAsync(username) {
+ //     try {
+ //         let response = await fetch(`https://api.github.com/users/${username}`);
+ //         let events = await response.json();
+ //         console.log(events);
+ //     }
+ //     catch(err) {
+ //         console.log(err);
+ //     }
+ // }
+
  const wait = ms => {
      return new Promise(response => {
          setTimeout(() => {
@@ -14,4 +26,3 @@
  }
  wait(1000).then(message => console.log(message));
  wait(3000).then(message => console.log(message));
- 
---
+++ js/restful-api.js
@@ -0,0 +1,94 @@
+// API -- APPLICATION PROGRAMMING INTERFACE
+// A SET OF RULES DEFINING HOW APPLICATIONS AND/OR DEVICES CAN CONNECT TO EACH OTHER AND COMMUNICATE WITH ONE ANOTHER
+
+// REST MEANS REPRESENTATIONAL STATE TRANSFER.
+// A PATTERN OF DESIGN PRINCIPLES FOR API'S
+// REST API'S COMMUNICATE VIA HTTP REQUESTS
+
+//IN A REST API, COMMUNICATION VIA HTTP REQUESTS IS USED TO PERFORM THE STANDARD SET OF ACTIONS CALLED CRUD
+// CREATE, READ, UPDATE, DELETE
+// JSON JAVASCRIPT OBJECT NOTATION
+
+// https://glitch.com/continuous-shard-yew
+
+const booksURL = "https://continuous-shard-yew.glitch.me/books"
+const moviesURl = "https://continuous-shard-yew.glitch.me/movies"
+
+// the R in crud : read
+function getMovies(){
+    fetch("https://continuous-shard-yew.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data));
+}
+
+getMovies();
+
+// the C in crud: create
+const bookToPost = {
+    title: "Eleanor of Aquitaine",
+    author: {
+        firstName: "Ralph",
+        lastName: "Turner"
+    }
+}
+
+const postOptions = {
+    method: "POST",
+    headers: {
+        'Content-type' : 'application/json'
+    },
+    body: JSON.stringify(bookToPost)
+}
+
+function getBooks(){
+    fetch(booksURL).then(resp => resp.json()).then(data => console.log(data));
+}
+
+getBooks();
+
+// fetch(booksURL, postOptions).then(getBooks);
+
+// The U in crud: updating with put and patch request
+// we'll use PUT to replace the entire content 
+// we'll use PATCH to modify only part of the entry
+
+let modification = {
+    title: "Eleanor of Aquitaine: Queen of France, Queen of England"
+}
+
+const patchOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-type' : 'application/json'
+    },
+    body: JSON.stringify(modification)
+}
+
+// fetch(booksURL + "/1", patchOptions).then(getBooks);
+
+modification = {
+    title: "Eleanor of Aquitaine and the Four Kings",
+    author: {
+        firstName: "Amy",
+        lastName: "Kelly"
+    }
+}
+
+const putOptions = {
+    method: 'PUT',
+    headers: {
+        'Content-type' : 'application/json'
+    },
+    body: JSON.stringify(modification)
+}
+
+// fetch(booksURL + "/1", putOptions).then(getBooks);
+
+// the D in crud: delete
+
+const deleteOptions = {
+    method: "DELETE",
+    headers: {
+        'Content-type' : 'applications/json'
+    }
+}
+
+fetch(booksURL + "/1", deleteOptions).then(getBooks);
