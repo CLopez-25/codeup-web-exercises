8b75f3f4259d6aa0d1c0620a9b7e0b44dbb93aa7
Cesar Lopez <lopez.m.cesar17@gmail.com>
2023-01-30 21:14:51 -0600

    going over for loops and fundamentals

--- js/for-loops.js
+++ js/for-loops.js
@@ -2,43 +2,45 @@
 // let number = 7;
 // console.log(number + " x + 1 = " * number);
 // console.log(number + " x + 2 = " * number * 2);
-//
-// function showMultiplicationTable() {
-//     let number = prompt("Enter a number:");
-//     for (let multiplier = 1; multiplier <= 10; multiplier++) {
-//         console.log(`${number} x ${multiplier} = ${multiplier * number}`);
-//     }
-// }
-// showMultiplicationTable();
-//
-// for (let i = 0; i < 10; i++){
-//     let randomNumber = Math.floor(Math.random() * 180) + 20;
-//     if (randomNumber % 2 === 0){
-//         console.log(randomNumber + " is even");
-//     } else {
-//         console.log(randomNumber + " is odd");
-//     }
-// }
 
-// for (let i = 1; i <= 9; i++){
-//     console.log(i.toString().repeat(i));
-// }
+// practicing for loops
+
+function showMultiplicationTable() {
+    let number = prompt("Enter a number:");
+    for (let multiplier = 1; multiplier <= 10; multiplier++) {
+        console.log(`${number} x ${multiplier} = ${multiplier * number}`);
+    }
+}
+showMultiplicationTable();
+
+for (let i = 0; i < 10; i++){
+    let randomNumber = Math.floor(Math.random() * 180) + 20;
+    if (randomNumber % 2 === 0){
+        console.log(randomNumber + " is even");
+    } else {
+        console.log(randomNumber + " is odd");
+    }
+}
+
+for (let i = 1; i <= 9; i++){
+    console.log(i.toString().repeat(i));
+}
 
 // loop within a loop below for the problem above:
 
-// for (let i = 1; i <= 9; i++){
-//     let outputString = "";
-//     for (let count = 0; count < i; count++){
-//         outputString = outputString + i;
-//     }
-//     console.log(outputString);
-// }
+for (let i = 1; i <= 9; i++){
+    let outputString = "";
+    for (let count = 0; count < i; count++){
+        outputString = outputString + i;
+    }
+    console.log(outputString);
+}
 // ^ outer loop is controlling how many lines are running
 // ^ inner loop controls how many number are in the lines
 
 
-// for(let count = 100; count >= 5; count -= 5){
-//     console.log(count);
-// }
+for(let count = 100; count >= 5; count -= 5){
+    console.log(count);
+}
 
 
